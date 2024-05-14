5c3dd6d972c16db7ecc12039adad6dba1602ab0f
n2sy <nidhal.jelassi@fsegt.utm.tn>
2024-05-14 20:58:06 +0100

    add @if and @for

--- src/app/cv/cv.component.ts
+++ src/app/cv/cv.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { Candidat } from '../models/candidat';
 
 @Component({
   selector: 'app-cv',
   templateUrl: './cv.component.html',
-  styleUrl: './cv.component.css'
+  styleUrl: './cv.component.css',
 })
 export class CvComponent {
+  tabCandidats: Candidat[] = [
+    new Candidat(1, 'Homer', 'Simpson', 30, 'ingénieur', 'homer.png'),
+    new Candidat(2, 'bart', 'Simpson', 23, 'cloud', 'bart.jpeg'),
+    new Candidat(3, 'lisa', 'Simpson', 27, 'designer', 'lisa.png'),
+  ];
+  selectedCandidat: Candidat;
 
+  recupererCandidat(cand) {
+    this.selectedCandidat = cand;
+  }
 }
--- src/app/liste/liste.component.ts
+++ src/app/liste/liste.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Candidat } from '../models/candidat';
 
 @Component({
   selector: 'app-liste',
   templateUrl: './liste.component.html',
-  styleUrl: './liste.component.css'
+  styleUrl: './liste.component.css',
 })
 export class ListeComponent {
-
+  @Input() allCandidats: Candidat[] = [];
+  @Output() candToCv = new EventEmitter();
+  sendCandToCv(cand) {
+    this.candToCv.emit(cand);
+  }
 }
